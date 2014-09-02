7447c6a9f94c0efe8591410c0a8c028dc499da2f
Jack Morrison <jackmorrison1@gmail.com>
2014-09-02 16:19:34 -0600

    Initial commit

---
+++ drafter.py
@@ -0,0 +1,21 @@
+import requests
+import re
+
+def run_loop():
+    """Do the damn thing.
+
+    """
+    while True:
+        pass
+        # Get selection time
+        # Sleep until selection time
+        # Find next position lacking a player
+        # Search for players of that position matching da
+        # Filter by first name matching Da
+        # Choose player from teams in order of priority
+        # Make pick
+        # Record that position was picked
+
+
+if __name__ == '__main__':
+    run_loop()
